0e12c422eec7a10fa43577104a7d28cec59697c2
Teemu Taskula <themetor@gmail.com>
2020-07-18 14:22:23 +0300

    Make mock tests pass

--- test/react-modal-sheet.test.tsx
+++ test/react-modal-sheet.test.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../src';
+import { Sheet } from '../src';
+
+// TODO: add tests
 
 describe('it', () => {
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Thing />, div);
+    ReactDOM.render(
+      <Sheet isOpen onClose={() => {}}>
+        Mock sheet test
+      </Sheet>,
+      div
+    );
     ReactDOM.unmountComponentAtNode(div);
   });
 });
