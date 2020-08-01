2d506904b7b528135533b9388177961506bfd4a2
Teemu Taskula <themetor@gmail.com>
2020-08-01 11:17:26 +0300

    Update examples

--- example/components/App.tsx
+++ example/components/App.tsx
@@ -1,6 +1,14 @@
 import * as React from 'react';
 import styled from 'styled-components';
 import { Routes, Route, Link } from 'react-router-dom';
+import { FaScroll as ScrollIcon } from 'react-icons/fa';
+import { MdAccessibility as A11yIcon } from 'react-icons/md';
+
+import {
+  AiFillApple as AppleIcon,
+  AiOutlineSlack as SlackIcon,
+  AiOutlineControl as SnapIcon,
+} from 'react-icons/ai';
 
 import { Screen, DarkMode } from './common';
 import SnapPoints from './SnapPoints';
@@ -69,18 +77,74 @@ const App = () => {
 const ExampleSelector = () => {
   return (
     <ExampleLinks>
-      <Link to="snap-points">Snap points</Link>
-      <Link to="scrollable">Scrollable</Link>
-      <Link to="apple-music">Apple Music</Link>
-      <Link to="slack-message">Slack Message</Link>
-      <Link to="a11y">Accessible Sheet</Link>
+      <li>
+        <ExampleLink to="snap-points">
+          <SnapIcon size={48} />
+          <span>Snap points</span>
+        </ExampleLink>
+      </li>
+
+      <li>
+        <ExampleLink to="scrollable">
+          <ScrollIcon size={48} />
+          <span>Scrollable</span>
+        </ExampleLink>
+      </li>
+
+      <li>
+        <ExampleLink to="apple-music">
+          <AppleIcon size={48} />
+          <span>Apple Music</span>
+        </ExampleLink>
+      </li>
+
+      <li>
+        <ExampleLink to="slack-message">
+          <SlackIcon size={48} />
+          <span>Slack Message</span>
+        </ExampleLink>
+      </li>
+
+      <li>
+        <ExampleLink to="a11y">
+          <A11yIcon size={48} />
+          <span>Accessible Sheet</span>
+        </ExampleLink>
+      </li>
     </ExampleLinks>
   );
 };
 
-const ExampleLinks = styled.div`
+const ExampleLinks = styled.ul`
+  display: grid;
+  grid-gap: 16px;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-template-rows: auto;
+  width: 100%;
+  max-width: 800px;
+  padding: 16px;
+`;
+
+const ExampleLink = styled(Link)`
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 32px 48px;
+  text-align: center;
+  background-color: aliceblue;
+  color: mediumslateblue;
+  border: 1px solid mediumslateblue;
+  border-radius: 12px;
+
+  span {
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  & > * + * {
+    margin-top: 16px;
+  }
 `;
 
 export default App;
