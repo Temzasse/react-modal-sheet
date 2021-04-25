3e821174b796fd0adfda5a2930dc91dc9a92dad2
Markus <markuswendorf@web.de>
2021-04-26 01:06:27 +0200

    ttes

--- src/sheet.tsx
+++ src/sheet.tsx
@@ -166,14 +166,10 @@ const Sheet = React.forwardRef<any, SheetProps>(
 
     const sheet = (
       <SheetContext.Provider value={context}>
-        <AnimatePresence>
-          {/* NOTE: AnimatePresence requires us to set keys to children */}
-          {isOpen
-            ? React.Children.map(children, (child: any, i) =>
-                React.cloneElement(child, { key: `sheet-child-${i}` })
-              )
-            : null}
-        </AnimatePresence>
+        {/* NOTE: AnimatePresence requires us to set keys to children */}
+        {React.Children.map(children, (child: any, i) =>
+          React.cloneElement(child, { key: `sheet-child-${i}` })
+        )}
       </SheetContext.Provider>
     );
 
