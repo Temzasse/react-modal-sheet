7c6d96c1a9a3dace3ae5363627b8d5f1f687d891
Markus <markuswendorf@web.de>
2021-05-01 13:32:35 +0200

    add ssr tests

---
+++ test/ssr.test.tsx
@@ -0,0 +1,41 @@
+/**
+ * @jest-environment node
+ */
+
+import * as React from 'react';
+import { renderToString } from 'react-dom/server';
+import Sheet from '../src';
+
+describe('Test with SSR enabled', () => {
+  it('renders <Sheet.Content> in html output', () => {
+    const content = 'Hello world';
+    const markup = renderToString(
+      <Sheet isOpen onClose={() => {}} ssr>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{content}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    );
+
+    expect(markup).toContain(content);
+  });
+});
+
+describe('Test with SSR disabled', () => {
+  it('does not render <Sheet.Content> in html output', () => {
+    const content = 'Hello world';
+    const markup = renderToString(
+      <Sheet isOpen onClose={() => {}}>
+        <Sheet.Container>
+          <Sheet.Header />
+          <Sheet.Content>{content}</Sheet.Content>
+        </Sheet.Container>
+        <Sheet.Backdrop />
+      </Sheet>
+    );
+
+    expect(markup).not.toContain(content);
+  });
+});
