829dcd6beddc2f78d2b83481b521f54a54976f37
Teemu Taskula <themetor@gmail.com>
2023-06-19 22:43:23 +0300

    Remove ios modal effect highlight

--- src/utils.ts
+++ src/utils.ts
@@ -15,8 +15,6 @@ export function getClosest(nums: number[], goal: number) {
   return closest;
 }
 
-const highlightId = 'react-modal-sheet-highlight';
-
 export function applyRootStyles(rootId: string) {
   const body = document.querySelector('body') as HTMLBodyElement;
   const root = document.querySelector(`#${rootId}`) as HTMLDivElement;
@@ -28,33 +26,17 @@ export function applyRootStyles(rootId: string) {
     body.style.backgroundColor = '#000';
     root.style.overflow = 'hidden';
     root.style.willChange = 'transform';
-    root.style.transition = 'transform 200ms linear';
+    root.style.transition =
+      'transform 200ms ease-in-out, border-radius 200ms linear';
     root.style.transform = `translateY(calc(env(safe-area-inset-top) + ${p / 2}px)) scale(${s})`; // prettier-ignore
     root.style.borderTopRightRadius = '10px';
     root.style.borderTopLeftRadius = '10px';
-
-    // Add highlighed overlay to emphasize the modality effect
-    const highlight = document.createElement('div');
-    highlight.setAttribute('aria-hidden', 'true');
-    highlight.id = highlightId;
-    highlight.style.position = 'absolute';
-    highlight.style.top = '0px';
-    highlight.style.left = '0px';
-    highlight.style.bottom = '0px';
-    highlight.style.right = '0px';
-    highlight.style.opacity = '0';
-    highlight.style.transition = 'opacity 200ms linear';
-    highlight.style.backgroundColor = 'rgba(150, 150, 150, 0.1)';
-
-    root.appendChild(highlight);
-    requestAnimationFrame(() => (highlight.style.opacity = '1'));
   }
 }
 
 export function cleanupRootStyles(rootId: string) {
   const body = document.querySelector('body') as HTMLBodyElement;
   const root = document.getElementById(rootId) as HTMLDivElement;
-  const highlight = document.getElementById(highlightId) as HTMLDivElement;
 
   function onTransitionEnd() {
     root.style.removeProperty('overflow');
@@ -62,15 +44,13 @@ export function cleanupRootStyles(rootId: string) {
     root.style.removeProperty('transition');
     body.style.removeProperty('background-color');
     root.removeEventListener('transitionend', onTransitionEnd);
-    highlight.parentNode?.removeChild(highlight);
   }
 
-  if (root && highlight) {
+  if (root) {
     // Start animating back
     root.style.removeProperty('border-top-right-radius');
     root.style.removeProperty('border-top-left-radius');
     root.style.removeProperty('transform');
-    highlight.style.opacity = '0';
 
     // Remove temp properties after animation is finished
     root.addEventListener('transitionend', onTransitionEnd);
