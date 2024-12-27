458ac2818243bbfccdf1b3b2619675531825800d
Teemu Taskula <themetor@gmail.com>
2024-12-27 11:41:24 +0200

    Omit lib from example's vite deps optimization

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   cacheDir: '.vite',
+  optimizeDeps: {
+    exclude: ['react-modal-sheet'],
+  }
 });
